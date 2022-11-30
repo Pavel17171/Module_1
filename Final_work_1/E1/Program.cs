834cdb4f674545f158558f7f5120d34e339a7f93
[1] <[1.asus.rfrf]>
2022-11-30 12:38:45 +0300

    Add folder E1 with code

---
+++ Final_work_1/E1/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] array = {"hello", "2", "world", ":-)"};
+int numberOfCharacters = 3;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= numberOfCharacters)
+    {
+        count++;
+    }
+}
+
+char s = '"';
+Console.Write($"Исходный массив: [{s}");
+Console.Write(String.Join($"{s},{s}", array));
+Console.WriteLine($"{s}]");
+
+string[] newArray = new string [count];
+int count2 = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= numberOfCharacters)
+    {
+        newArray[count2] = array[i];       
+        count2++;
+    }
+}
+
+Console.Write($"Результат выборки: [{s}");
+Console.Write(String.Join($"{s},{s}", newArray));
+Console.Write($"{s}]");
